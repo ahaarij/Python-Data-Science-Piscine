23c1f6cf2c2ba701f931c4c133d0fef213e1e85d
Ahmed Aarij <ahaarij@student.42abudhabi.ae>
2026-03-25 21:11:34 +0400

    pushing ex03

---
+++ module00/ex03/NULL_not_found.py
@@ -0,0 +1,16 @@
+def NULL_not_found(object: any) -> int:
+    if object == None:
+        print(f"Nothing: {object} {type(object)}")
+    elif isinstance(object, float) and object != object: # nan 
+        print(f"Cheese: {object} {type(object)}")
+    elif isinstance(object, bool) and object is False:
+        print(f"Fake: {object} {type(object)}")
+    elif isinstance(object, int) and object == 0:
+        print(f"Zero: {object} {type(object)}")
+    elif isinstance(object, str) and len(object) == 0:
+        print(f"Empty: {type(object)}")
+    else:
+        print(f"Type not Found")
+        return 1
+    return 0
+
