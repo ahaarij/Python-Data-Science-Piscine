21f11d484cef4eaca05c269d4667038293140fca
Ahmed Aarij <ahaarij@student.42abudhabi.ae>
2026-03-30 23:16:24 +0400

    pushing added numpy to venv, and finished mex01

--- module01/ex01/array2D.py
+++ module01/ex01/array2D.py
@@ -1,3 +1,16 @@
-
+import numpy as np
 
 def slice_me(family: list, start: int, end: int) -> list:
+    try:
+        if not isinstance(family, list) \
+         or not isinstance(start, int) \
+         or not isinstance(end, int):
+            raise AssertionError("Input must be a list and two integers")
+        if not (len(item) == len(family) for item in family):
+            raise AssertionError("Input list with different sizes")
+        print(f"My shape is : {np.array(family).shape}")
+        print(f"My new shape is : {np.array(family)[start:end].shape}")
+        return np.array(family)[start:end].tolist()
+    except AssertionError as e:
+        print("Assertion Error: ", e)
+        
